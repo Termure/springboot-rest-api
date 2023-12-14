5c0f300431a2b200d090e8126739ae5b7c778964
Ioan Termure <ioan.termure24@gmail.com>
2023-12-14 19:28:09 +0200

    Replaces constructor with lombok annotation

--- src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot.service.impl;
 
+import lombok.AllArgsConstructor;
 import net.javaguides.springboot.entity.Post;
 import net.javaguides.springboot.mapper.PostMapper;
 import net.javaguides.springboot.payload.PostDto;
@@ -8,13 +9,11 @@ import net.javaguides.springboot.service.PostService;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
 
-    public PostServiceImpl(PostRepository postRepository){
-        this.postRepository = postRepository;
-    }
     @Override
     public PostDto createPost(PostDto postDto){
         Post savedPost = postRepository.save(PostMapper.mapToPost(postDto));
