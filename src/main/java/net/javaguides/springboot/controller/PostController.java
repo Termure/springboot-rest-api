3f4c0eb245c303f3b1f31e5192b2470b69970291
Ioan Termure <ioan.termure24@gmail.com>
2023-12-14 19:18:42 +0200

    Implements create post resource

--- src/main/java/net/javaguides/springboot/controller/PostController.java
+++ src/main/java/net/javaguides/springboot/controller/PostController.java
@@ -1,7 +1,24 @@
 package net.javaguides.springboot.controller;
 
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.payload.PostDto;
+import net.javaguides.springboot.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/api/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
 }
