02328923ce3f67785a82f60267ad53e3b01cfe72
Ioan Termure <ioan.termure24@gmail.com>
2023-12-14 19:21:53 +0200

    Implements create post service actions

--- src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/impl/PostServiceImpl.java
@@ -1,23 +1,24 @@
 package net.javaguides.springboot.service.impl;
 
-import lombok.AllArgsConstructor;
 import net.javaguides.springboot.entity.Post;
+import net.javaguides.springboot.mapper.PostMapper;
 import net.javaguides.springboot.payload.PostDto;
 import net.javaguides.springboot.repository.PostRepository;
 import net.javaguides.springboot.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostServiceImpl implements PostService {
 
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public PostServiceImpl(PostRepository postRepository){
         this.postRepository = postRepository;
     }
     @Override
     public PostDto createPost(PostDto postDto){
-        return null;
+        Post savedPost = postRepository.save(PostMapper.mapToPost(postDto));
+
+        return PostMapper.mapToPostDto(savedPost);
     }
 }
