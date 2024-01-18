88167aec1523f67832699884eca0145121eb3170
Ioan Termure <ioan.termure24@gmail.com>
2024-01-18 17:10:03 +0200

    Adds Comment dto class and jpa repository

---
+++ src/main/java/net/javaguides/springboot/payload/CommentDto.java
@@ -0,0 +1,11 @@
+package net.javaguides.springboot.payload;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+    private long id;
+    private String email;
+    private String name;
+    private String body;
+}
