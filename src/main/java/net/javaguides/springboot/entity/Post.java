d1705fc5849e9013e1c81566d99b12fc6e011f44
Ioan Termure <ioan.termure24@gmail.com>
2024-04-03 14:21:23 +0300

    Implements ManyToOne relationship between posts and category

--- src/main/java/net/javaguides/springboot/entity/Post.java
+++ src/main/java/net/javaguides/springboot/entity/Post.java
@@ -31,6 +31,10 @@ public class Post {
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
     public Post(long id, String title, String description, String content) {
         this.id = id;
         this.title = title;
